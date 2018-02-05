2394bae254302e27338e557c5346f80ce845e711
minhpham <minh.pham@dhigroupinc.com>
2018-02-05 11:53:25 -0600

    modify name

--- src/main/java/com/dice/aws/lambda/LambdaRequestHandler.java
+++ src/main/java/com/dice/aws/lambda/LambdaRequestHandler.java
@@ -1,14 +1,13 @@
 package com.dice.aws.lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-
+import org.joda.time.DateTime;
 
 public class LambdaRequestHandler
   implements RequestHandler<String, String> {
     public String handleRequest(String input, Context context) {
         context.getLogger().log("Input: " + input);
-        return "Hello World";
-        /*DateTime dateTime = new DateTime();
-        return "Hello World: " + input +". Current dateTime is: "+ dateTime.toString();*/
+        DateTime dateTime = new DateTime();
+        return "Hello World 2nd version: " + input +". Current dateTime is: "+ dateTime.toString();
     }
 }
